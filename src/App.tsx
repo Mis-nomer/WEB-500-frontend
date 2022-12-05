4d7bcb158075392ab248f6b24d115b3714f9fec8
Nadirol <99162550+Nadirol@users.noreply.github.com>
2022-12-05 16:45:52 +0700

    added auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home'
 import Admin from './components/admin/Dashboard'
+import SignUp from './components/auth/SignUp'
+import Login from './components/auth/Login'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route key='home' path='/' element={<Home />}></Route>
         <Route key='admin' path='/admin' element={<Admin />}></Route>
+        <Route key='sign-up' path='/sign-up' element={<SignUp />}></Route>
+        <Route key='login' path='/login' element={<Login />}></Route>
       </Routes>
     </>
   )
